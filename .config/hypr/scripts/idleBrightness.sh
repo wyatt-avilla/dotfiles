44ff372980946d2a9b0aa00f837d4317381f7d2e
wyatt-avilla <wyattmurphy1@gmail.com>
2024-03-20 11:55:54 -0700

    hyprland script dir creation

---
+++ .config/hypr/scripts/idleBrightness.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+systemBrightnessFile="/sys/class/backlight/acpi_video0/brightness"
+prevBrightnessFile="/tmp/prevBrightness.tmp"
+
+
+if [ "$1" = "startIdle" ]; then
+    cat $systemBrightnessFile > $prevBrightnessFile
+    cat $prevBrightnessFile
+    echo "0" | doas tee $systemBrightnessFile
+fi
+
+if [ "$1" = "endIdle" ]; then
+    cat $prevBrightnessFile | doas tee $systemBrightnessFile
+fi
+
+if [ "$1" = "finalIdle" ]; then
+    if ! pgrep -x "emerge" > /dev/null && ! pgrep -x "make" > /dev/null; then
+    loginctl suspend
+    fi
+fi
