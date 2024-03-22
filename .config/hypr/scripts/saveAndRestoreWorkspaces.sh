6e46ecb29d983a6a7a220e94b1684688843b90dd
wyatt-avilla <wyattmurphy1@gmail.com>
2024-03-21 17:01:46 -0700

    adjust script location in `saveAndRestoreWorkspaces.sh`

--- .config/hypr/scripts/saveAndRestoreWorkspaces.sh
+++ .config/hypr/scripts/saveAndRestoreWorkspaces.sh
@@ -6,7 +6,7 @@ if [ "$1" = "save" ]; then
 	killall -9 swayidle
 	hyprctl monitors -j | jq -r '.[] | "\(.name):\(.activeWorkspace.id)"' >$workspaceFile
 	hyprctl dispatch focusmonitor HDMI-A-1 && hyprctl dispatch workspace 68 && hyprctl dispatch focusmonitor DP-1 && hyprctl dispatch workspace 69
-	swayidle timeout 1 "went idle..." resume "$HOME/.config/hypr/saveAndRestoreWorkspaces.sh restore"
+	swayidle timeout 1 "echo went idle..." resume "$HOME/.config/hypr/scripts/saveAndRestoreWorkspaces.sh restore"
 fi
 
 if [ "$1" = "restore" ]; then
@@ -16,7 +16,7 @@ if [ "$1" = "restore" ]; then
 			hyprctl dispatch focusmonitor "$monitorName" && hyprctl dispatch workspace "$prevWorkspace"
 		done <"$workspaceFile"
 		hyprctl dispatch focusmonitor DP-1
-		swayidle timeout 600 "$HOME/.config/hypr/saveAndRestoreWorkspaces.sh save"
+		swayidle timeout 600 "$HOME/.config/hypr/scripts/saveAndRestoreWorkspaces.sh save"
 	else
 		echo "File does not exist."
 	fi
